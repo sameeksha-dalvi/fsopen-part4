06b68829c45cb7b1a67e782c205a33290ac1e242
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-16 18:04:14 +0530

    feat: move routes to controllers (exercise 4.2)

--- bloglist/index.js
+++ bloglist/index.js
@@ -1,7 +1,7 @@
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const express = require('express')
-const Blog = require('./models/blog')
+const notesRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -12,26 +12,9 @@ mongoose.connect(mongoUrl, { family: 4 })
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+app.use('/api/blogs', notesRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  })
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
