2872f8d37a8f128455f07e1f550c9d3719ebea0e
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-17 17:45:23 +0530

    feat: implement totalLikes function and add tests for empty, single, and multiple blogs (exercise 4.4)

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) =>{
+    const result = blogs.reduce((total,current) =>{
+        return total + current.likes
+    },0)
+
+    return result
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
