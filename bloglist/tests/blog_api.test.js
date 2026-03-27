9f0a38902db82acb142d01f1d215a16d9847ad95
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-27 13:55:20 +0530

    test: add GET /api/blogs test and refactor route to async/await (exercise 4.8)

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -1,17 +1,48 @@
-const {test, after} = require('node:test');
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test');
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async() =>{
-    await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type',/application\/json/)
+const initialBlogs = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+    },
+    {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5
+    }
+]
+
+beforeEach(async() =>{
+    await Blog.deleteMany({})
+    let blogObject =  new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject =  new Blog(initialBlogs[1])
+    await blogObject.save()
 })
 
-after(async() =>{
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async() =>{
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
     await mongoose.connection.close()
 })
