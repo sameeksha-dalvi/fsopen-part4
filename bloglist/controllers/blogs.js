136ddee01e709d4e76a19388c8eef476c5eae090
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-04-02 17:05:16 +0530

    feat: add update blog functionality and tests (exercise 4.14)

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('express')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -13,7 +14,7 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  if(body.title === undefined || body.url === undefined){
+  if (body.title === undefined || body.url === undefined) {
     return response.status(400).end()
   }
   const likesValue = body.likes === undefined ? 0 : body.likes;
@@ -33,9 +34,27 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request , response) =>{
+blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).end()
+  }
+
+  if (title !== undefined) blog.title = title
+  if (author !== undefined) blog.author = author
+  if (url !== undefined) blog.url = url
+  if (likes !== undefined) blog.likes = likes
+
+  const updatedBlog = await blog.save()
+  response.json(updatedBlog)
+
 })
 
 module.exports = blogsRouter
--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -115,6 +115,29 @@ test('a blog can be deleted', async() => {
     assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
 })
 
+
+test('a blog likes can be updated', async () => {
+  const blogsAtStart = await api.get('/api/blogs')
+  const blogToUpdate = blogsAtStart.body[0]
+
+  const updatedBlog = {
+    title: blogToUpdate.title,
+    author: blogToUpdate.author,
+    url: blogToUpdate.url,
+    likes: blogToUpdate.likes + 1
+  }
+
+  await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(updatedBlog)
+    .expect(200)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+  const result = blogsAtEnd.body.find(b => b.id === blogToUpdate.id)
+
+  assert.strictEqual(result.likes, blogToUpdate.likes + 1)
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
