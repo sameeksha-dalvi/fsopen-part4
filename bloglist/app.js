e5fefb1dbb540567c8414644c914e0233f4d213d
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-06-26 17:10:09 +0530

    feat: install and configure jsonwebtoken and add login route with token authentication (exercise 4.18)

--- bloglist/app.js
+++ bloglist/app.js
@@ -3,6 +3,7 @@ const config = require('./utils/config')
 const express = require('express')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const mongoose = require('mongoose')
@@ -26,6 +27,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
